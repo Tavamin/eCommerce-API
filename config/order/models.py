4bc17a41e7a285abbb33b99fc5ad432c9d2e44b4
Amin Tavakoli <46283482+Tavamin@users.noreply.github.com>
2022-12-09 11:42:43 +0330

    Add | Product and Order models

---
+++ config/order/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+from product.models import Product, Discount
+
+
+# Create your models here.
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    cost = models.FloatField()
+    ORDER_STATUS_CHOICES = [('pending', 'Pending'), ('processing', 'Processing'), ('shipped', 'Shipped'),
+                            ('delivered', 'Delivered')]
+
+    status = models.CharField(max_length=50, choices=ORDER_STATUS_CHOICES, default='pending')
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='orders')
+    discount = models.ForeignKey(Discount, on_delete=models.CASCADE, related_name='orders', null=True, blank=True)
+
+
+    def __str__(self):
+        return f"{self.user} - {self.status}"
+
+
+    class Meta:
+        ordering = ['created_at']
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='items')
+    quantity = models.IntegerField()
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.product} - {self.quantity}"
+
+
+
+
+class Cart(models.Model):
+    price = models.FloatField()
+    discount = models.ForeignKey(Discount, on_delete=models.CASCADE, related_name='carts', null=True, blank=True)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='carts')
+
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='cart_items')
+    quantity = models.IntegerField()
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.product} - {self.quantity}"
+
+    class Meta:
+        ordering = ['product']
---
+++ config/product/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+
+
+# Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField()
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='children', null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+
+class Discount(models.Model):
+    DISCOUNT_CHOICES = [('percent', 'Percent'), ('amount', 'Amount')]
+    kind = models.CharField(max_length=50, choices=DISCOUNT_CHOICES, default='percent')
+    value = models.FloatField()
+    voucher = models.CharField(max_length=90, unique=True, null=True, blank=True)
+    expiry_date = models.DateTimeField()
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='discounts', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.kind} - {self.value}"
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=80)
+    slug = models.SlugField()
+    description = models.TextField()
+    price = models.FloatField(max_length=18)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='products')
+    discounts = models.ManyToManyField(Discount)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='comments', null=True, blank=True)
+
+    def __str__(self):
+        return f" {self.product} - {self.user}"
+
+    class Meta:
+        ordering = ['content']
+
+
+
